0847228ed2e888ec4adf14015f998207bccb2dbb
cmdada <cmdada@proton.me>
2024-08-29 14:26:21 -0600

    seperated drivetrain out into its own file

---
+++ src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,43 @@
+package frc.robot;
+
+import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+
+public class Drivetrain {
+    private DifferentialDrive m_robotDrive;
+
+    // Motors
+    private final PWMVictorSPX m_leftMotor1 = new PWMVictorSPX(0);
+    private final PWMSparkMax m_leftMotor2 = new PWMSparkMax(1);
+    private final PWMSparkMax m_rightMotor1 = new PWMSparkMax(2);
+    private final PWMVictorSPX m_rightMotor2 = new PWMVictorSPX(3);
+
+    public Drivetrain() {
+        // Group the motors
+        MotorControllerGroup leftMotors = new MotorControllerGroup(m_leftMotor1, m_leftMotor2);
+        MotorControllerGroup rightMotors = new MotorControllerGroup(m_rightMotor1, m_rightMotor2);
+
+        // Invert the right side motors (depends on wiring)
+        rightMotors.setInverted(true);
+
+        // Setup DifferentialDrive
+        m_robotDrive = new DifferentialDrive(leftMotors, rightMotors);
+
+        // Register motors
+        SendableRegistry.addChild(m_robotDrive, m_leftMotor1);
+        SendableRegistry.addChild(m_robotDrive, m_leftMotor2);
+        SendableRegistry.addChild(m_robotDrive, m_rightMotor1);
+        SendableRegistry.addChild(m_robotDrive, m_rightMotor2);
+    }
+
+    public DifferentialDrive getDrive() {
+        return m_robotDrive;
+    }
+
+    public void arcadeDrive(double forwardBackward, double leftRight) {
+        m_robotDrive.arcadeDrive(forwardBackward, leftRight);
+    }
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,50 +1,24 @@
-// This is where our code actually goes
 package frc.robot;
 
-import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 
 public class Robot extends TimedRobot {
-  private DifferentialDrive m_robotDrive;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
+    private Drivetrain m_drivetrain;
+    private Joystick m_leftStick;
+    private Joystick m_rightStick;
 
-  // all the motors
-  private final PWMVictorSPX m_leftMotor1 = new PWMVictorSPX(0);
-  private final PWMSparkMax m_leftMotor2 = new PWMSparkMax(1);
-  private final PWMSparkMax m_rightMotor1 = new PWMSparkMax(2);
-  private final PWMVictorSPX m_rightMotor2 = new PWMVictorSPX(3);
+    @Override
+    public void robotInit() {
+        m_drivetrain = new Drivetrain();
+        m_leftStick = new Joystick(0);  // Left stick for forward/backward
+        m_rightStick = new Joystick(1); // Right stick for left/right turning
+    }
 
-  @Override
-  public void robotInit() {
-    // Group the motors
-    MotorControllerGroup leftMotors = new MotorControllerGroup(m_leftMotor1, m_leftMotor2);
-    MotorControllerGroup rightMotors = new MotorControllerGroup(m_rightMotor1, m_rightMotor2);
-
-    // Register motors
-    SendableRegistry.addChild(m_robotDrive, m_leftMotor1);
-    SendableRegistry.addChild(m_robotDrive, m_leftMotor2);
-    SendableRegistry.addChild(m_robotDrive, m_rightMotor1);
-    SendableRegistry.addChild(m_robotDrive, m_rightMotor2);
-
-    // Invert the right side motors (depends on wiring)
-    rightMotors.setInverted(true);
-
-    // Setup DifferentialDrive
-    m_robotDrive = new DifferentialDrive(leftMotors, rightMotors);
-    m_leftStick = new Joystick(0);  // Left stick for forward/backward
-    m_rightStick = new Joystick(1); // Right stick for left/right turning like a drone controller thingy idk its more intuitive for me
-  }
-
-  @Override
-  public void teleopPeriodic() {
-    double forwardBackward = -m_leftStick.getY(); // Forward/backward with left stick Y-axis
-    double leftRight = m_rightStick.getX();       // Left/right turning with right stick X-axis
-    m_robotDrive.arcadeDrive(forwardBackward, leftRight);
-  }
+    @Override
+    public void teleopPeriodic() {
+        double forwardBackward = -m_leftStick.getY(); // Forward/backward with left stick Y-axis
+        double leftRight = m_rightStick.getX();       // Left/right turning with right stick X-axis
+        m_drivetrain.arcadeDrive(forwardBackward, leftRight);
+    }
 }
