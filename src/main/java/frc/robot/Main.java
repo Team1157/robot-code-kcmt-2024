79df0111b4280b7faabad276d4b8597cdcba2e99
cmdada <cmdada@proton.me>
2024-08-29 14:05:41 -0600

    wasd is left stick and arrow keys are right stick

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -1,7 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
+// Don't edit this file without asking ada
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,7 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
+// This is where our code actually goes
 package frc.robot;
 
 import edu.wpi.first.util.sendable.SendableRegistry;
