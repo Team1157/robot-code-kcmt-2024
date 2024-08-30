58d7201f815ab9acaf291dac6c99e1a8509d24fd
landsharks <82778523+landsharks@users.noreply.github.com>
2024-08-29 22:50:27 -0600

    made it all on one joystick, after testing it makes more sense

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,24 +1,23 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
     private Drivetrain m_drivetrain;
-    private Joystick m_leftStick;
-    private Joystick m_rightStick;
+    private Joystick m_joystick;
 
     @Override
     public void robotInit() {
         m_drivetrain = new Drivetrain();
-        m_leftStick = new Joystick(0);  // Left stick for forward/backward
-        m_rightStick = new Joystick(1); // Right stick for left/right turning
+        m_joystick = new Joystick(0);  // Use joystick on port 0
     }
 
     @Override
     public void teleopPeriodic() {
-        double forwardBackward = -m_leftStick.getY(); // Forward/backward with left stick Y-axis
-        double leftRight = m_rightStick.getX();       // Left/right turning with right stick X-axis
+        double forwardBackward = -m_joystick.getRawAxis(1); // Forward/backward with left stick Y-axis (index 1)
+        double leftRight = m_joystick.getRawAxis(0);       // Left/right turning with left stick X-axis (index 0)
         m_drivetrain.arcadeDrive(forwardBackward, leftRight);
     }
 }
